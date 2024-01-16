6003ef39bac2b2476ba900c45831cc2bbf7e164c
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2024-01-17 03:14:32 +0700

    bug fixing

---
+++ frontend/app/profile-page/page.js
@@ -0,0 +1,12 @@
+import Header from "../../component/group/header";
+import "./page.css";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <section className="article-section"></section>
+      <footer className="footer"></footer>
+    </main>
+  );
+}
