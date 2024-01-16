10e69d97200d949ef7ff1a77447bbc50a7392e92
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2024-01-17 03:37:30 +0700

    buzz space

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,13 @@
 import Header from "../component/group/header/index";
 import CardSection from "../component/group/cardsection/index";
 import "./page.css";
+import HeaderDesc from "../component/single/headerdesc";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <HeaderDesc />
       <CardSection />
       <section className="article-section"></section>
       <footer className="footer"></footer>
---
+++ frontend/component/single/headerdesc/index.js
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+export default function HeaderDesc({ summary }) {
+  return (
+    <div className="summary">
+      <Typewriter
+        onInit={(Typewriter) => {
+          Typewriter.pauseFor(2500)
+            .changeDelay(50)
+            .typeString(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi libero, finibus ut ligula eu, condimentum imperdiet libero. Quisque tincidunt efficitur purus, vel finibus elit laoreet non. Phasellus scelerisque feugiat nisl eu fermentum."
+            )
+            .callFunction(() => {
+              console.log("String typed out!");
+            })
+            .start();
+        }}
+      />
+    </div>
+  );
+}
