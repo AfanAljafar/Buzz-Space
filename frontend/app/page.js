80843bc2ab038f4385f54c1e4b2eecc8bf8acc1c
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2024-01-20 20:09:34 +0700

    buzz space

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,13 @@
 import Header from "../component/group/header/index";
-
 import "./page.css";
 import Footer from "../component/group/footer";
+import NewsPaper from "../component/group/newspaper";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <NewsPaper />
       <Footer />
     </main>
   );
---
+++ frontend/component/group/newspaper/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./np.css";
+
+export default function NewsPaper() {
+  return (
+    <div className="bg-np">
+      <div className="content-np"> under development</div>
+    </div>
+  );
+}
